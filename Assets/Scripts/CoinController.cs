bb25c78427e8c9534bc99e0b3458c4fef6c44f3d
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-19 19:05:21 +0900

    CoinController追加

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    private CoinModel _model;
+
+    public int Score { get { return _model.Score; } }
+    public float Scale { get { return _model.Scale; } }
+}
+
+public class CoinModel
+{
+    public int Score { get; private set; }
+    public float Scale { get; private set; }
+
+    public CoinModel(int score, float scale)
+    {
+        Score = score;
+        Scale = scale;
+    }
+}
