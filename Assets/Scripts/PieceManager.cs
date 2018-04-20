621ba7a47bd74044855a9ac33d94b24189cb2c4f
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-20 16:19:08 +0900

    PieceManagerをSingleton化、設定されたピースをランダムで生成して返す処理

--- Assets/Scripts/PieceManager.cs
+++ Assets/Scripts/PieceManager.cs
@@ -1,8 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class PieceManager : MonoBehaviour
+public class PieceManager : SingletonMonoBehaviour<PieceManager>
 {
     private List<PieceController> _bornPieceList = new List<PieceController>();
+
+    public void CreateAtRandom(Transform parent, Action<PieceController> onCreate)
+    {
+        var index = UnityEngine.Random.Range(0, _bornPieceList.Count - 1);
+        var prefab = _bornPieceList[index];
+        PieceController piece = Instantiate(prefab, parent);
+
+        onCreate(piece);
+    }
 }
