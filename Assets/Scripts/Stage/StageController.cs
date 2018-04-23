091ca8c4774835faec3bc05c89c127e2a17a5667
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-23 12:25:44 +0900

    生成位置の縦軸もずらすように修正

--- Assets/Scripts/Stage/StageController.cs
+++ Assets/Scripts/Stage/StageController.cs
@@ -8,7 +8,10 @@ public class StageController : MonoBehaviour
     private Transform _bornPointOrigin;
 
     [SerializeField]
-    private float _bornPosRadius = 1f;
+    private float _bornPosRadiusX = 7f;
+
+    [SerializeField]
+    private float _bornPosRadiusY = 2f;
 
 	private void Start()
 	{
@@ -23,8 +26,10 @@ public class StageController : MonoBehaviour
     private void OnCreatePiece(PieceController piece)
     {
         var startPos = _bornPointOrigin.localPosition;
-        var randomizeBuffX = Random.Range(-_bornPosRadius, _bornPosRadius);
+        var randomizeBuffX = Random.Range(-_bornPosRadiusX, _bornPosRadiusX);
+        var randomizeBuffY = Random.Range(-_bornPosRadiusY, _bornPosRadiusY);
         startPos.x += randomizeBuffX;
+        startPos.y += randomizeBuffY;
 
         piece.transform.localPosition = startPos;
     }
