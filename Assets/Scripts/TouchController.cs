3899aa70a963adeaf2cc696cfae5266b5c1f4c32
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-24 17:44:49 +0900

    Ray飛ばす処理と早期リターン

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -4,26 +4,42 @@ using UnityEngine;
 
 public class TouchController : MonoBehaviour
 {
-    public DragPieceController DragPiece { get; private set; }
+    public DragPiece DragPiece { get; private set; }
 
 	private void Awake()
 	{
-        DragPiece = new DragPieceController();
+        DragPiece = new DragPiece();
 	}
 
 	private void Update()
 	{
         if(Input.GetMouseButtonDown(0))
         {
-            DragPiece.OnDragStart();
+            var ray = GetRaycast();
+            DragPiece.OnDragStart(ray);
         }
 	}
+
+    private RaycastHit2D GetRaycast()
+    {
+        return Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+    }
 }
 
-public class DragPieceController
+public class DragPiece
 {
-    public void OnDragStart()
+    public void OnDragStart(RaycastHit2D hit)
     {
-        Debug.Log("ドラッグ開始");
+        if (hit.collider == null) return;
+        if (hit.collider.gameObject == null) return;
+
+        var hitGO = hit.collider.gameObject;
+        if(hitGO.CompareTag("Piece"))
+        {
+            Debug.Log("ドラッグ開始 go: " + hitGO);
+        } else
+        {
+            // 何もしない
+        }
     }
 }
