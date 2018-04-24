aa509168fbdd70ea4cdc603f67bd6d4031d8e7dd
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-24 17:54:05 +0900

    PieceViewの設定

--- Assets/Scripts/Piece/PieceController.cs
+++ Assets/Scripts/Piece/PieceController.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
+[RequireComponent(typeof(PieceView))]
 public class PieceController : MonoBehaviour
 {
+    private PieceView _view;    
     private PieceModel _model;
 
     public int Score { get { return _model.Score; } }
     public float Scale { get { return _model.Scale; } }
+
+	private void Awake()
+	{
+        _view = GetComponent<PieceView>();
+
+        Assert.IsNotNull(_view);
+	}
+
+    public void SetColor(Color color)
+    {
+        _view.SetColor(color);
+    }
 }
---
+++ Assets/Scripts/Piece/PieceView.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceView : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer _renderer;
+
+    public void SetColor(Color color)
+    {
+        _renderer.color = color;
+    }
+}
