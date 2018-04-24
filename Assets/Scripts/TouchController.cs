308a8ebc3dc05ba02ff76a4ec3768a1647738037
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-24 16:34:40 +0900

    タッチ処理クラス作成

---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+    public DragPieceController DragPiece { get; private set; }
+
+	private void Awake()
+	{
+        DragPiece = new DragPieceController();
+	}
+
+	private void Update()
+	{
+        if(Input.GetMouseButtonDown(0))
+        {
+            DragPiece.OnDragStart();
+        }
+	}
+}
+
+public class DragPieceController
+{
+    public void OnDragStart()
+    {
+        Debug.Log("ドラッグ開始");
+    }
+}
